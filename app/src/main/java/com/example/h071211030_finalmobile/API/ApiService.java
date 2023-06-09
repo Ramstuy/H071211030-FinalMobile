2bac5135bd944e197fe2a755664c33186396c1d7
Ramstuy <ikrhidayat@gmail.com>
2023-06-09 20:25:59 +0800

    added the other TvShowsResponse to ApiService

--- app/src/main/java/com/example/h071211030_finalmobile/API/ApiService.java
+++ app/src/main/java/com/example/h071211030_finalmobile/API/ApiService.java
@@ -11,15 +11,24 @@ public interface ApiService {
     @GET("movie/now_playing?api_key=39725c0f5856c3e5447f0d93ffc805fc")
     Call<MovieResponse> getNowPlaying();
 
-    @GET("movie/popular")
-    Call<MovieResponse> getPopular(@Query("api_key") String api_key);
+    @GET("movie/popular?api_key=39725c0f5856c3e5447f0d93ffc805fc")
+    Call<MovieResponse> getPopularMovie(@Query("api_key") String api_key);
 
-    @GET("movie/top_rated")
-    Call<MovieResponse> getTopRated(@Query("api_key") String api_key);
+    @GET("movie/top_rated?api_key=39725c0f5856c3e5447f0d93ffc805fc")
+    Call<MovieResponse> getTopRatedMovie(@Query("api_key") String api_key);
 
-    @GET("movie/upcoming")
+    @GET("movie/upcoming?api_key=39725c0f5856c3e5447f0d93ffc805fc")
     Call<MovieResponse> getUpcoming(@Query("api_key") String api_key);
 
     @GET ("tv/airing_today?api_key=39725c0f5856c3e5447f0d93ffc805fc")
     Call<TvShowsResponse> getAiringToday();
+
+    @GET ("tv/on_the_air?api_key=39725c0f5856c3e5447f0d93ffc805fc")
+    Call<TvShowsResponse> getOnTheAir();
+
+    @GET ("tv/popular?api_key=39725c0f5856c3e5447f0d93ffc805fc")
+    Call<TvShowsResponse> getPopularTv();
+
+    @GET ("tv/top_rated?api_key=39725c0f5856c3e5447f0d93ffc805fc")
+    Call<TvShowsResponse> getTopRatedTv();
 }
